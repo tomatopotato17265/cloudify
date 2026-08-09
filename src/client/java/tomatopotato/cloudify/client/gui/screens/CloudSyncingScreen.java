ac71ea339dc144f392940f6d3d4fcc0affded276
tomato potato <nimaigos108@gmail.com>
2026-08-09 08:16:04 -0700

    feat: add Done button to cloud syncing options

--- src/client/java/tomatopotato/cloudify/client/gui/screens/CloudSyncingScreen.java
+++ src/client/java/tomatopotato/cloudify/client/gui/screens/CloudSyncingScreen.java
@@ -1,9 +1,11 @@
 package tomatopotato.cloudify.client.gui.screens;
 
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.HeaderAndFooterLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 
@@ -20,6 +22,7 @@ public class CloudSyncingScreen extends Screen {
 	@Override
 	protected void init() {
 		this.layout.addTitleHeader(TITLE, this.font);
+		this.layout.addToFooter(Button.builder(CommonComponents.GUI_DONE, button -> this.onClose()).width(200).build());
 		this.layout.visitWidgets(this::addRenderableWidget);
 		this.repositionElements();
 	}
@@ -34,6 +37,8 @@ public class CloudSyncingScreen extends Screen {
 		super.extractRenderState(graphics, mouseX, mouseY, partialTick);
 		Identifier headerSeparator = this.minecraft.level == null ? Screen.HEADER_SEPARATOR : Screen.INWORLD_HEADER_SEPARATOR;
 		graphics.blit(RenderPipelines.GUI_TEXTURED, headerSeparator, 0, this.layout.getHeaderHeight() - 2, 0.0F, 0.0F, this.width, 2, 32, 2);
+		Identifier footerSeparator = this.minecraft.level == null ? Screen.FOOTER_SEPARATOR : Screen.INWORLD_FOOTER_SEPARATOR;
+		graphics.blit(RenderPipelines.GUI_TEXTURED, footerSeparator, 0, this.height - this.layout.getFooterHeight(), 0.0F, 0.0F, this.width, 2, 32, 2);
 	}
 
 	@Override
