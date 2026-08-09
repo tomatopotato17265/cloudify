a689a616c779b5474d110779adbd36493c394592
tomato potato <nimaigos108@gmail.com>
2026-08-08 22:51:44 -0700

    Initial commit

---
+++ src/client/java/tomatopotato/cloudify/client/CloudifyClient.java
@@ -0,0 +1,10 @@
+package tomatopotato.cloudify.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CloudifyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
