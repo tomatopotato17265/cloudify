c5b3f6c982184ad758bdf42b73263c308eba241e
tomato potato <nimaigos108@gmail.com>
2026-08-09 08:22:33 -0700

    feat: add background tint to cloud syncing options

--- src/client/java/tomatopotato/cloudify/client/gui/screens/CloudSyncingScreen.java
+++ src/client/java/tomatopotato/cloudify/client/gui/screens/CloudSyncingScreen.java
@@ -10,6 +10,8 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 
 public class CloudSyncingScreen extends Screen {
+	private static final Identifier MENU_LIST_BACKGROUND = Identifier.withDefaultNamespace("textures/gui/menu_list_background.png");
+	private static final Identifier INWORLD_MENU_LIST_BACKGROUND = Identifier.withDefaultNamespace("textures/gui/inworld_menu_list_background.png");
 	private static final Component TITLE = Component.literal("Cloud Syncing Options");
 	private final HeaderAndFooterLayout layout = new HeaderAndFooterLayout(this, 33);
 	private final Screen lastScreen;
@@ -34,7 +36,13 @@ public class CloudSyncingScreen extends Screen {
 
 	@Override
 	public void extractRenderState(final GuiGraphicsExtractor graphics, final int mouseX, final int mouseY, final float partialTick) {
+		int contentTop = this.layout.getHeaderHeight();
+		int contentBottom = this.height - this.layout.getFooterHeight();
+		Identifier menuListBackground = this.minecraft.level == null ? MENU_LIST_BACKGROUND : INWORLD_MENU_LIST_BACKGROUND;
+		graphics.blit(RenderPipelines.GUI_TEXTURED, menuListBackground, 0, contentTop, 0.0F, 0.0F, this.width, contentBottom - contentTop, 32, 32);
+
 		super.extractRenderState(graphics, mouseX, mouseY, partialTick);
+
 		Identifier headerSeparator = this.minecraft.level == null ? Screen.HEADER_SEPARATOR : Screen.INWORLD_HEADER_SEPARATOR;
 		graphics.blit(RenderPipelines.GUI_TEXTURED, headerSeparator, 0, this.layout.getHeaderHeight() - 2, 0.0F, 0.0F, this.width, 2, 32, 2);
 		Identifier footerSeparator = this.minecraft.level == null ? Screen.FOOTER_SEPARATOR : Screen.INWORLD_FOOTER_SEPARATOR;
