6a94cf60ca70a05b20da4ab248bd8543d0987a84
tomato potato <nimaigos108@gmail.com>
2026-08-09 18:56:01 -0700

    fix: gitignore the client ID and secret

---
+++ src/client/java/tomatopotato/cloudify/client/drive/GoogleDriveConfig.java
@@ -0,0 +1,25 @@
+package tomatopotato.cloudify.client.drive;
+
+import com.google.api.services.drive.DriveScopes;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.util.List;
+import java.util.Properties;
+
+public class GoogleDriveConfig {
+	public static final String CLIENT_ID;
+	public static final String CLIENT_SECRET;
+	public static final List<String> SCOPES = List.of(DriveScopes.DRIVE_FILE);
+
+	static {
+		Properties properties = new Properties();
+		try (InputStream stream = GoogleDriveConfig.class.getResourceAsStream("/cloudify-google-drive.properties")) {
+			properties.load(stream);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+		CLIENT_ID = properties.getProperty("client_id");
+		CLIENT_SECRET = properties.getProperty("client_secret");
+	}
+}
